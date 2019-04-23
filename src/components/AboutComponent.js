f20a2a57ff2897287ce1a87fd0eedc852f2cb567
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-04-23 16:56:53 -0400

    About us correction

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { RenderLeader } from './RenderLeaderComponent';
 
 function RenderLeader ({leader}) {
   return (
