037194563f40ff4745c97ba10e4730e04ca37e22
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-05-03 16:57:38 -0400

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, Button, ModalHeader, ModalBody, Label, Row, Col} from "reactstrap";
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -127,7 +128,25 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
   function RenderComments({comments, addComment, dishId}) {
 
-    if (comments != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         const listOfComments = comments.map((comment) => {
             return (
               <ul className="list-unstyled">
