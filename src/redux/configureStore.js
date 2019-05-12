6f2859f9d986bd0ed67731d724fcc3740bad031e
David Giraldo <dagibu007@gmail.com>
2019-05-12 17:57:12 -0400

    react-redux-form-revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,6 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
@@ -9,12 +11,15 @@ import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders
-        }),
+      combineReducers({
+          dishes: Dishes,
+          comments: Comments,
+          promotions: Promotions,
+          leaders: Leaders,
+          ...createForms({
+              feedback: InitialFeedback
+          })
+      }),
         applyMiddleware(thunk, logger)
     );
 
